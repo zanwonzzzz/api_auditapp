908cee9903d3ed37c036933e8bb3e7306bbf5fa3
zanwonzzzz <etnicastro9@gmail.com>
2025-06-30 18:01:00 -0600

    cuarto commit

--- app/v1/controllers/consultas.py
+++ app/v1/controllers/consultas.py
@@ -1,19 +1,42 @@
-from databases import Database
+from fastapi import FastAPI
+import aiomysql
 from dotenv import load_dotenv
-
+import os
+from fastapi.responses import JSONResponse
 load_dotenv()
+#consultas
+#esta tabla va a estar guardada en cache para filtrarla en android
 
-database = Database(
-    db_database = os.getenv('DB_DATABASE')
-    db_host = os.getenv('DB_HOST')
-    db_username = os.getenv('DB_USERNAME')
-    db_password = os.getenv('DB_PASSWORD')
-    db_port = os.getenv('DB_PORT')
-)
+async def conexion():
+    conn = await aiomysql.connect(host=os.getenv('DB_HOST'), 
+                                port=int(os.getenv('DB_PORT')),
+                                    user=os.getenv('DB_USERNAME'), 
+                                    password=os.getenv('DB_PASSWORD'),
+                                        db=os.getenv('DB_DATABASE'))
+    return conn                              
+async def OrdenesSinAuditor():
+    conn = await conexion()
+    cur = await conn.cursor()
+    await cur.execute("SELECT tic.Folio_Pisa,d.Division,ad.Nombre_Auditor,ad.Apellido_Auditor,c.COPE,a.area,tic.Distrito,tic.Tecnologia,tic.Terminal,tic.Tipo_Instalacion,tic.Metraje,tic.FK_Auditor,ads.Asigno FROM tecnico_instalaciones_coordiapp tic LEFT JOIN copes c ON FK_Cope = c.id JOIN areas a ON c.FK_Area = a.idAreas JOIN divisiones d ON a.FK_Division = d.idDivision LEFT JOIN Auditores ad ON tic.FK_Auditor = ad.idAuditor LEFT JOIN Auditorias_Det ads ON tic.Folio_Pisa = ads.Folio_Pisa WHERE tic.Estatus_Orden = 'COMPLETADA' AND (tic.FK_Auditor = 0 OR ads.Estatus_Auditoria = 'PENDIENTE') AND tic.Step_Registro = 5 ORDER BY tic.Distrito ASC")
+    print(cur.description)
+    r = await cur.fetchall()
+    print(r)
+    await cur.close()
+    conn.close()
 
-await database.connect()
-
-def consulta():
-    query = "SELECT * FROM copes"
-    rows = await database.fetch_all(query=query)
-    return ('Copes:', rows)
+#consultas del dashboard de auditorias
+async def graficasAuditorias(inicio,fin,estatus_auditoria):
+        conn = await conexion()
+        cur = await conn.cursor()
+        sql = "SELECT COUNT(DISTINCT Folio_Pisa) as total_auditorias FROM Auditorias_Det WHERE `Estatus_Auditoria` = %s AND `Fecha_Inicio` >= %s AND `Fecha_Inicio` < %s"
+        await cur.execute(sql,(inicio,fin,estatus_auditoria))
+        print(cur.description)
+        r = await cur.fetchall()
+        await cur.close()
+        conn.close()
+        return JSONResponse (
+            content= {
+                'total_auditorias':r
+            },
+            status_code=200
+        )
--- app/v1/routers/routes.py
+++ app/v1/routers/routes.py
@@ -3,7 +3,7 @@ import aiomysql
 from dotenv import load_dotenv
 import os
 from fastapi.responses import JSONResponse
-
+from app.v1.controllers.consultas import *
 
 app = FastAPI()
 load_dotenv()
@@ -24,8 +24,11 @@ async def test_example():
     await cur.close()
     conn.close()
 
-#consultas simples 
-
+#consultas del dashboard de auditorias
+#no puedo llamar directamente a una funcion debe estar dentro de otra funcion
+@app.get("/total/{inicio}/{fin}/{estatus_auditoria}")
+async def endpoint_graficasAuditorias(inicio, fin, estatus_auditoria):
+    return await graficasAuditorias(inicio, fin, estatus_auditoria)
 
 
  
